7e38d1445fd4d3ed0f576a9a674e97958449433b
Mondal10 <amitmondal111.am@gmail.com>
2019-11-17 21:13:02 +0530

    Basic functionality of webapp

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+console.log('Hello');
+
+const input = document.getElementById('lbsInput');
+const output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
+
+input.addEventListener('input', (e) => {
+  let lbs = e.target.value;
+
+  output.style.visibility = 'visible';
+
+  let grams = document.getElementById('gOutput');
+  grams.innerHTML = lbs / 0.0022046;
+
+  let kiloGrams = document.getElementById('kgOutput');
+  kiloGrams.innerHTML = lbs / 2.2046;
+
+  let ounces = document.getElementById('ozOutput');
+  ounces.innerHTML = lbs * 16;
+
+  e.preventDefault();
+});
